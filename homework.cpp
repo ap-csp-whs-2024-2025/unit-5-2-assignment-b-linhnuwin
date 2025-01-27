4c6deab4965c9f9590bbf6a9a664de2c9938d27c
linhnuwin <linhnguyen091208@gmail.com>
2025-01-27 00:13:17 +0000

    List #2

--- homework.cpp
+++ homework.cpp
@@ -8,12 +8,40 @@ void displayList(std::vector<T> const &v);
 int main()
 {
     // You can follow along with any code in class here
+    std::vector<std::string> Players = {"Peter", "Bruce", "Steve", "Tony", "Natasha", "Clint", "Wanda", "Hope", "Danny", "Carol"};
+    std::vector<int> Numbers = {100, 50, 10, 1, 2, 7, 11, 17, 53, -8, -4, -9, -72, -64, -80};
 
-    
+    //Problem #1
+    int curr_index = 0
+    while (curr_index < Players.size())
+    {
+        std::cout << Players[1 curr_index] << " ";
+        curr_index = curr_index + 2;
+    }
+
+    //Problem #2
+    int curr_index = 0;
+    while (curr_index < Numbers.size())
+    {
+        if (Numbers[curr_index] > 0)
+        {
+            std::cout << Numbers[curr_index] << " ";
+        }
+        curr_index = curr_index + 1;
+    }
+    std::cout << std::endl;
     return 0;
 }
 
-
+// Problem #3
+int curr_index = 0;
+int sum = 0
+while (curr_index < Numbers.size())
+{
+    sum += numbers[curr_index];
+    curr_index++;
+}
+std::cout << "The sum of all numbers in the list is: " << sum << std::endl;
 
 
 
